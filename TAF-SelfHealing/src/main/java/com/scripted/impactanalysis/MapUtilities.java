2329bc1bf40f1941929f8bea39705e7d0addcb7f
Ganesh236701 <132676674+Ganesh236701@users.noreply.github.com>
2023-05-30 07:10:45 -0700

    Delete TAF-SelfHealing directory

--- TAF-SelfHealing/src/main/java/com/scripted/impactanalysis/MapUtilities.java
+++
@@ -1,9 +0,0 @@
-package com.scripted.impactanalysis;
-
-public class MapUtilities {
-
-	public static void main(String[] args) throws Exception {
-
-		impactReportGeneration obj = new impactReportGeneration("");
-	}
-}
